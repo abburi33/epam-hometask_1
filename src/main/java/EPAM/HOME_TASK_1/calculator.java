e86ba48257d51dfc4b5825ec06f25fde13d0fac4
abburi33 <abburisahithi33@gmail.com>
2021-02-15 23:24:37 +0530

    I've created a simple calculator app.

--- src/main/java/EPAM/HOME_TASK_1/calculator.java
+++ src/main/java/EPAM/HOME_TASK_1/calculator.java
@@ -1,6 +1,7 @@
 package EPAM.HOME_TASK_1;
 import java.util.*;
-public class calculator {
+public class calculator 
+{
 
 	public static void main(String[] args)
 	{
@@ -8,13 +9,31 @@ public class calculator {
         int a,b;
         a=sc.nextInt();
         b=sc.nextInt();
-        System.out.println("Addition is "+(a+b));
-        System.out.println("Substraction is "+(a-b));
-        System.out.println("Multiplication is "+(a*b));
+        System.out.println("Addition is "+add(a,b));
+        System.out.println("Substraction is "+sub(a,b));
+        System.out.println("Multiplication is "+mul(a,b));
         if(b!=0)
-        System.out.println("Division is "+(a/b));
+        System.out.println("Division is "+div(a,b));
         else
         	System.out.println("Invalid input");
 	}
-
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	
+	public static int sub(int a,int b)
+	{
+		return a-b;
+	}
+	
+	public static int mul(int a,int b)
+	{
+		return a*b;
+	}
+	
+	public static int div(int a,int b)
+	{
+		return a/b;
+	}
 }
